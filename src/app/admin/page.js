3ecef3d6bebe9edc3c2e9671d34e13d95ca40720
ffjaervik <fabianfjaervik@gmail.com>
2023-05-30 16:27:40 +0100

    signup & signin helper functions

---
+++ src/app/admin/page.js
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import { useAuthContext } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+function Page() {
+  const { user } = useAuthContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user == null) router.push("/");
+  }, [user]);
+
+  return <h1>Only logged in users can view this page</h1>;
+}
+
+export default Page;
